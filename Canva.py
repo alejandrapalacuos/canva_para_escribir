040298ee6afa417d901800d09b3225f1892378ed
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-10-03 09:52:01 -0500

    Create Canva.py

---
+++ Canva.py
@@ -0,0 +1,25 @@
+import streamlit as st
+st.title('Dibujitos')
+
+# Add canvas component
+# Specify canvas parameters in application
+st.sidebar.drawing_mode = st.selectbox(
+    "freedraw",
+    ("circle", "rectangle"),
+)
+st.sidebar.stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+st.sidebar.stroke_color = st.color_picker("Pick A Color", "#00f900", "000000", "ffffff")
+bg_color = '#000000'
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+
+  
+  
